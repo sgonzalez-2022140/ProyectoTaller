94bc1ff6ff21dc8159ab85a2b7752fdf2e7fb1e8
sgonzalez-2022140 <sgonzalez-2022140@kinal.edu.gt>
2024-03-04 11:44:29 -0600

    Se cambio el nombre de token por authorization en la validación

--- ProyectoFinal1erBim/src/middlewares/validate-jwt.js
+++ ProyectoFinal1erBim/src/middlewares/validate-jwt.js
@@ -8,11 +8,11 @@ export const validateJwt = async(req, res, next)=>{
         //Obtener la llave de acceso al token
         let secretKey = process.env.SECRET_KEY
         //obtener el token de los headers
-        let { token } = req.headers
+        let { authorization } = req.headers
         //Verificar si viene el token
-        if(!token) return res.status(401).send({message: 'Unauthorized'})
+        if(!authorization) return res.status(401).send({message: 'Unauthorized'})
         //Obtener el uid del usuario que envió el token
-        let { uid } = jwt.verify(token, secretKey)
+        let { uid } = jwt.verify(authorization, secretKey)
         //Validar si aún existe en la BD
         let user = await User.findOne({_id: uid})
         if(!user) return res.status(404).send({message: 'User not found - Unauthorized'})
@@ -27,7 +27,7 @@ export const validateJwt = async(req, res, next)=>{
 export const isAdmin = async(req, res, next)=>{
     try{
         let { user } = req
-        if(!user || user.role !== 'ADMIN') return res.status(403).send({message: `You dont have access | username: ${user.username}`})
+        if(!user || user.role !== 'ADMIN') return res.status(403).send({message: `You dont have access `})
         next()
     }catch(err){
         console.error(err)
